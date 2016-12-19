e87be0e9e3057972be75dae73c2f998e489decf7
Juju <gsshamgunova@gmail.com>
2016-12-19 11:48:43 +0300

    Update TradesJob.java

--- homeWork3/src/main/java/com/company/TradesJob.java
+++ homeWork3/src/main/java/com/company/TradesJob.java
@@ -6,25 +6,25 @@ import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 public class TradesJob {
-
     private final TradesDAO tradesDAO;
+    private final Downloader dd;
+    private final Parser<String> s;
 
-    public TradesJob(TradesDAO tradesDAO) {
+    public TradesJob(TradesDAO tradesDAO, DummyDownloader dd, DummyParser s) {
         this.tradesDAO = tradesDAO;
+        this.dd = dd;
+        this.s = s;
     }
-
+    
     public void run() throws IOException {
         //Downloader d = new FTPDownloader();
         //String f = (String)d.downloadTradesFile();
-        Downloader dd = new DummyDownloader();
         dd.print();
 
         //Parser<Trade> p = new CSVParser();
         //Iterable<Trade> trades = p.parse(f);
-        Parser<String> s = new DummyParser();
         s.print();
-
-        TradesDAO td = new TradesDAO();
+        tradesDAO.deleteAll();
         //td.updateTrades(trades);
     }
 
